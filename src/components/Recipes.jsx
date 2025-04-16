f49b1536377ee72ca839c6ddbe820c1f84f14ec4
NatalieChurch <nataliechurch12@gmail.com>
2025-04-15 19:59:25 -0500

    Registration mostly created

--- src/components/Recipes.jsx
+++ src/components/Recipes.jsx
@@ -13,7 +13,14 @@ function Recipes (recipes, setRecipes, favRecipes, setFavRecipes) {
         {
             recipes.map((recipe) => 
                 <div key={recipe.idMeal}>
-                    <h2>{recipe.strMeal}</h2>
+                    <h1>{recipe.strMeal}</h1>
+                    <img>{recipe.strMealThumb}</img>
+                    <h2>{recipe.strCategory}</h2>
+                    <h2>{recipe.strArea}</h2>
+                    <h2>{recipe.ingredients}</h2>
+                    <h2>{recipe.strInstructions}</h2>
+                    <h2>{recipe.strYoutube}</h2>
+                    <h3>{recipe.strTags}</h3>
                     <button onClick={()=>handleClick(recipe)}>Favorite this Recipe</button>
                 </div>
             )
--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,5 +1,51 @@
+import { useState } from "react";
+
 function Register () {
 
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState(null);
+
+
+async function handleSubmit(event) {
+    event.preventDefault();
+    try {
+        const response = await fetch ("https://fsa-jwt-practice.herokuapp.com/signup",
+            {
+                method: "POST",
+                headers:{
+                    "Content-type": "application/json"
+                },
+                body: JSON.stringify({
+                    username: username,
+                    password: password
+                })
+            })
+
+            const result = await response.json()
+            console.log(result);
+            setToken(result.token);
+
+    } catch (error){
+        setError(error.message)
+    }
+}
+
+return(
+    <div>
+        <h2>Sign up now!</h2>
+        {error && <p>{error}</p>}
+        <form onSubmit={handleSubmit}>
+            <label>
+                <input value={username} onChange={(e) => setUsername(e.target.value)} placeholder="Enter username here" minLength={7} />
+            </label>
+            <label>
+                <p>Password: </p>
+                <input value={password} onChange={(e) => setPassword(e.target.value)} />
+            </label>
+            <button>Submit!</button>
+        </form>
+    </div>);
 }
 
 export default Register
